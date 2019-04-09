c3452443dc905ae3f447b025aa7847834ff072b2
Juzi <juzi214032@qq.com>
2019-04-09 11:38:41 +0800

    <feat>(项目结构)：初始化项目结构

---
+++ src/main/java/com/juzi/wage/WageApplication.java
@@ -0,0 +1,13 @@
+package com.juzi.wage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WageApplication.class, args);
+    }
+
+}
